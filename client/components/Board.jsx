fc54b8792282c1414041f92230fe7c847d40b937
Annika <liebste.anniu@gmaile.com>
2018-04-12 12:54:37 +1200

    adds initial set up Questions

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Score from './Score'
+import Questions from './Questions'
 
 
 // stateful as well
---
+++ client/components/Questions.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Questions = (props) => {
+
+    return 
+    <div className="questions">
+      {props.question}
+    </div>
+  }
+
+  // is this really props question?! 
+
+export default Questions
