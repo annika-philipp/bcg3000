008ba8f1a1dc0dd09c49f410fe423fe45ceb6cd1
Annika <liebste.anni@gmail.com>
2018-06-09 15:06:18 +1200

    starts enzyme testing addScore component

---
+++ tests/client/addScore.test.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import Enzyme, { shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter: new Adapter() })
+import AddScore from '../../client/components/AddScore'
+import ScoreBoard from '../../client/components/Scoreboard'
+
+test('Test suite working', () => {
+  expect(true).toBeTruthy
+})
+
+// Initial State
+test('initial state returns correctly', () => {
+  const totalscore = 100
+  const isTopScore = true
+  const topScores = [
+    {score: 100, name: 'Ben'},
+    {score: 110, name: 'Maddy'}
+  ]
+  const props = {
+    totalscore, isTopScore, topScores
+  }
+
+  const expected = {
+    name: '',
+    score: totalscore,
+    isTopScore: isTopScore
+  }
+  
+  const wrapper = shallow(<AddScore {...props} />)
+  const actual = wrapper.instance().state
+    expect(actual).toEqual(expected)
+})
+
+test('Text renders on AddScore', () => {
+  const topScores = [
+    {score: 100, name: 'Ben'},
+    {score: 110, name: 'Maddy'}
+  ]
+  const wrapper = shallow(<AddScore topScores={topScores} />)
+  expect(wrapper.find('h3').first().text()).toBe('Well done!')
+  expect(wrapper.find('h3').last().text()).toBe('Deploy yourself into phase 3')
+})
+
+test('handleChange adds to state', () => {
+  const topScores = [
+    {score: 100, name: 'Ben'},
+    {score: 110, name: 'Maddy'}
+  ]
+
+  const initialState = {
+    name: ''
+  }
+  const testEvent = {
+    target: {
+      name: 'name',
+      value: 'Ben'
+    }
+  }
+  const expected = {
+    name: testEvent.target.value
+  }
+
+  const wrapper = shallow(<AddScore topScores={topScores}/>)
+  wrapper.instance().render = () => <div></div>
+  wrapper.instance().state = initialState
+  wrapper.instance().handleChange(testEvent)
+
+  const actual = wrapper.instance().state
+  expect(actual).toEqual(expected)
+})
+
+// test works but throws a callback error - investigate!
+// test('addScore gets called on submit', () => {
+//   const topScores = [
+//     {score: 100, name: 'Ben'},
+//     {score: 110, name: 'Maddy'}
+//   ]
+
+//   const initialState = {
+//     name: 'Ben',
+//     score: 100,
+//     isTopScore: true
+//   }
+//   const expected = {
+//     ...initialState
+//   }
+//   const e = {
+//     preventDefault: () => ({})
+//   }
+//   const wrapper = shallow(<AddScore topScores={topScores} />)
+//   wrapper.instance().render = () => <div></div>
+//   wrapper.instance().state = initialState
+//   wrapper.instance().addScore(e)
+
+//   const actual = wrapper.instance().state
+//   expect(actual).toEqual(expected)
+// })
+
+test('render <ScoreBoard /> components', () => {
+  const topScores = [
+    {score: 100, name: 'Ben'},
+    {score: 110, name: 'Maddy'}
+  ]
+  const wrapper = shallow(<AddScore topScores={topScores} />)
+  expect(wrapper.find(ScoreBoard)).toHaveLength(1)
+})
