52ac9879f0f03de16cf788249713ec8c4b8e3ac9
Annika <liebste.anni@gmail.com>
2018-06-09 13:41:08 +1200

    adds react component testing

---
+++ tests/client/gameOver.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Enzyme, { shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter: new Adapter() })
+import GameOver from '../../client/components/GameOver'
+
+
+test('Text renders on GameOver', () => {
+  const wrapper = shallow(<GameOver />)
+  expect(wrapper.find('h3').first().text()).toBe('GAME OVER')
+  expect(wrapper.find('h3').last().text()).toBe('Consult your nearest handbook...')
+})
+
+test('Button text renders on GameOver', () => {
+  const wrapper = shallow(<GameOver />)
+  expect(wrapper.find('button').text()).toBe('Play again')
+})
