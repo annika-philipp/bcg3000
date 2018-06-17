f6740e2ddc83609904a987820a887c69d9496cd7
Annika <liebste.anni@gmail.com>
2018-06-17 16:02:27 +1200

    moves questionList function from routes to db

--- server/db/db.js
+++ server/db/db.js
@@ -12,7 +12,32 @@ function getQuestionsAndAnswers (id, testConn) {
   const conn = testConn || connection
   return conn('questions')
     .join('answers', 'questions.id', 'answers.question_id')
-    .select()
+    .select().then(formatQuestions)
+}
+
+// .select() returns an array where each question/answer pair is an object, total of 24
+// .then(formatQuestions) results in an array where each question is an object that has a nested array each for its two answers and two scores, total of 12
+
+function formatQuestions(questions) {
+  let questionsList = []
+  for (var i = 0; i < questions.length; i = i + 2) {
+    let question = questions[i]
+    let nextQuestion = questions[i + 1]
+    let q = {
+      question: question.question,
+      question_id: question.question_id,
+      answers: [
+        question.answer,
+        nextQuestion.answer
+      ],
+      scores: [
+        question.score,
+        nextQuestion.score
+      ]
+    }
+    questionsList.push(q)
+  }
+  return questionsList
 }
 
 function getScores (testConn) {
